3e0cbcf38bb988c48f8ae15d7952046a229acff2
hitesh07082002 <hiteshsadhwani777@gmail.com>
2025-02-19 02:51:57 +0530

    Configured axios with Vercel backend and built frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',  // ✅ Ensure correct backend URL
+  baseURL: 'todo-app-two-pi-72.vercel.app',  // ✅ Ensure correct backend URL
   withCredentials: true,
 });
 
